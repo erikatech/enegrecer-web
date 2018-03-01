6de262efddcf33fc2aa236783fb40c6b87d98eee
Daniel Faria <lenin@stack.com>
2018-03-01 17:42:47 -0300

    [Erika/Daniel] Primeiro teste unitário funcionando

---
+++ src/containers/denuncias/DenunciaContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+
+let ContactForm = props => {
+    const { envia } = props
+    return (
+      <form onSubmit={envia} noValidate>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" component="input" type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" component="input" type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Field name="email" component="input" type="email" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    )
+  }
+  
+  
+  let DenunciaContainer = reduxForm({
+    form: 'denuncia'
+  })(ContactForm)
+
+  export default connect(null, null)(DenunciaContainer);
---
+++ src/containers/denuncias/DenunciaContainer.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import DenunciaContainer from './DenunciaContainer';
+
+
+import { reducer as formReducer } from 'redux-form'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+
+
+describe('DenunciaContainer', () => {
+    let store
+	let envia
+	let subject
+	beforeEach(() => {
+		store = createStore(combineReducers({ form: formReducer }))
+		envia = jest.fn();
+		const props = {
+			envia,
+		}
+		// With redux-form v5, we could do <ContactFormContainer store={store}/>.
+		// However, with redux-form v6, the Field component we use is itself
+		// connected to the redux store. Therefore, we must put the store into
+		// context. To do that, we use <Provider/>.
+		subject = mount(
+			<Provider store={store}>
+				<DenunciaContainer {...props}/>
+            </Provider>
+             
+		)
+	})
+
+
+
+    it('com dados válidos não deve acontecer erros de validação', () => {
+        subject.find('form').simulate('submit');
+        console.log(subject.find('form'));
+        expect(envia).toHaveBeenCalled();
+    });
+}) 
